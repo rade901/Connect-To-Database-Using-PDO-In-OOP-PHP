ffb854c933e41d05f107db037da12f4941223fd0
Rade Jasenovčanin <78330407+rade901@users.noreply.github.com>
2021-09-01 05:44:47 +0200

    PDO data in table

--- dbc.php
+++ dbc.php
@@ -1,28 +1,12 @@
 <?php
 
-class dbh
-{
-
-    private $servername;
-    private $username;
-    private $password;
-    private $dbname;
-    private $charset;
-
-    public function connect()
-    {
-        $this->servername = "localhost";
-        $this->username = "root";
-        $this->password = "";
-        $this->dbname = "hefest_crud";
-        $this->charset = "utf8mb4";
-        try {
-            $dsn = "mysql:host=" . $this->servername . ";dbname=" . $this->dbname . ";charset=" . $this->charset;
-            $pdo = new PDO($dsn, $this->username, $this->password);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-        } catch (\Exception $e) {
-            echo "Connection faild: ".$e->getMessage();
-        }
-    }
+$dsn = 'mysql:host=localhost;dbname=hefest_crud';
+$username = 'root';
+$password ='';
+$options =[];
+try{
+    $connection = new PDO($dsn, $username, $password, $options);
+    echo 'connection successful';
+} catch(PDOException $e){
+    
 }
--- index.php
+++ index.php
@@ -1,18 +1,39 @@
 <?php
-include_once 'dbc.php';
+require 'dbc.php';
+$sql = 'SELECT * FROM records';
+$statment = $connection->prepare($sql);
+$statment->execute();
+$records = $statment->fetchAll(PDO::FETCH_OBJ);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Learning-PDO</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-   <?php
-   $object = new Dbh;
-   $object->connect();
-   ?>
+
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Ime</th>
+            <th>Prezime</th>
+        </tr>
+        <?php foreach($records as $record):?>
+        <tr>
+            <td><?= $record->Korisnik_ID;?></td>
+            <td><?= $record->Korisnik_Ime;?></td>
+            <td><?= $record->Korisnik_prezime;?></td>
+        </tr>
+        <?php endforeach;?>
+    </table>
+
 </body>
+
 </html>
